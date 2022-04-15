1448238d285ec1288320d08d8b6e514bfff3d353
mohorka <victoria2000dzuba@gmail.com>
2022-04-15 13:04:07 +0300

    add new extractors

--- core/data_preprocessing/features_extractions.py
+++ core/data_preprocessing/features_extractions.py
@@ -1,9 +1,11 @@
-from ast import In
-from keras.applications.vgg16 import VGG16, preprocess_input
-from keras.applications.vgg19 import VGG19, preprocess_input
 from keras.preprocessing.image import img_to_array
-from keras.applications.inception_resnet_v2 import InceptionResNetV2, preprocess_input
-from config.config import VGG_RESIZE, INCEPTIONRESNET_RESIZE
+from keras.applications.vgg16 import VGG16, preprocess_input as vgg16_input
+from keras.applications.vgg19 import VGG19, preprocess_input as vgg19_input
+from keras.applications.xception import Xception, preprocess_input as xception_input
+from keras.applications.resnet import ResNet50,  preprocess_input as resnet50_input
+from keras.applications.inception_v3 import InceptionV3, preprocess_input as inception_input
+from keras.applications.inception_resnet_v2 import InceptionResNetV2, preprocess_input as inception_resnet_input
+from config.config import VGG_RESIZE, INCEPTION_RESIZE, RESNET_RESIZE
 from keras.models import Model
 import cv2 as cv
 
@@ -11,7 +13,7 @@ def vgg16_extractor(img):
     img = cv.resize(img, VGG_RESIZE)
     img = img_to_array(img)
     img = img.reshape((1, img.shape[0], img.shape[1], img.shape[2]))
-    img = preprocess_input(img)
+    img = vgg16_input(img)
 
     model = VGG16()
     model = Model(inputs=model.inputs,outputs=model.layers[-2].output)
@@ -22,18 +24,54 @@ def vgg19_extractor(img):
     img = cv.resize(img, VGG_RESIZE)
     img = img_to_array(img)
     img = img.reshape((1, img.shape[0], img.shape[1], img.shape[2]))
-    img = preprocess_input(img)
+    img = vgg19_input(img)
 
     model = VGG19()
     model = Model(inputs=model.inputs,outputs=model.layers[-2].output)
     features = model.predict(img)
     return features
 
-def inception_resnet_extractor(img):
-    img = cv.resize(img, INCEPTIONRESNET_RESIZE)
+def xception_extractor(img):
+    img = cv.resize(img, INCEPTION_RESIZE)
     img = img_to_array(img)
     img = img.reshape((1, img.shape[0], img.shape[1], img.shape[2]))
-    img = preprocess_input(img)
+    img = xception_input(img)
+
+    model = Xception()
+    model = Model(inputs=model.inputs,outputs=model.layers[-2].output)
+    features = model.predict(img)
+    return features
+
+def resnet50_extractor(img):
+    img = cv.resize(img, RESNET_RESIZE)
+    img = img_to_array(img)
+    img = img.reshape((1, img.shape[0], img.shape[1], img.shape[2]))
+    img = resnet50_input(img)
+
+    model = ResNet50()
+    model = Model(inputs=model.inputs,outputs=model.layers[-2].output)
+    features = model.predict(img)
+    return features
+
+def inceptionv3_extractor(img):
+    img = cv.resize(img, INCEPTION_RESIZE)
+    img = img_to_array(img)
+    img = img.reshape((1, img.shape[0], img.shape[1], img.shape[2]))
+    img = inception_input(img)
+
+    model = InceptionV3()
+    model = Model(inputs=model.inputs,outputs=model.layers[-2].output)
+    features = model.predict(img)
+    return features
+
+
+
+
+def inception_resnet_extractor(img):
+    img = cv.resize(img, INCEPTION_RESIZE)
+    img = img_to_array(img)
+    img = img.reshape((1, img.shape[0], img.shape[1], img.shape[2]))
+    img = inception_resnet_input(img)
 
     model = InceptionResNetV2()
     model = Model(inputs=model.inputs,outputs=model.layers[-2].output)
